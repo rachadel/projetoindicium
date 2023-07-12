97fe634927767ff52a790033a1e1dc1d3b363be6
Daniel Rachadel <daniracha@gmail.com>
2023-07-12 01:53:16 +0000

    commit

---
+++ models/marts/dim_estados.sql
@@ -0,0 +1,21 @@
+with
+    stateprovince as (
+        select 
+            stateprovinceid as id_estado,
+            territoryid as id_territorio,
+            stateprovincecode as cd_estado,
+            name as nm_estado,
+            countryregioncode as cd_pais
+        from 
+            {{ ref("stg_raw__person_stateprovince") }}
+    ),
+    transformacao as (
+        select
+            row_number() over (order by id_estado) as sk_estado,
+            *,
+            current_date                           as dh_atualizacao
+        from 
+            stateprovince
+    )
+
+select * from transformacao
